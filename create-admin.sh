c502df2af7a113793402735bf70a36ba770db11f
Ruslan Semov <me@recluse.ru>
2017-12-21 19:08:32 +0300

    Add admin.txt

--- create-admin.sh
+++ create-admin.sh
@@ -11,13 +11,14 @@ echo $id
 if [[ -z "$id" ]]; then
   docker-compose exec dialog-server /opt/docker/bin/cli create-user -u $admin_user -n $admin_user
   id=`docker-compose exec postgresql su - postgres -c "echo \"select id,name from users where name='$admin_user'\" | psql -d dialog" | grep $admin_user | awk '{print $1}'`
-  docker-compose exec dialog-server /opt/docker/bin/cli admin-grant -u $id
+  docker-compose exec dialog-server /opt/docker/bin/cli admin-grant -u $id | grep "Admin granted. Password: " | tee admin.txt
 else
   while true; do
-    read -p "User $1 was created. Do generate admin password? (y/n): " yn
+    read -t 10 -p "User $1 was created. Do generate admin password? (y/n): " yn
     case $yn in
-      [Yy]* ) docker-compose exec dialog-server /opt/docker/bin/cli admin-grant -u $id; break;;
+      [Yy]* ) docker-compose exec dialog-server /opt/docker/bin/cli admin-grant -u $id | grep "Admin granted. Password: " | tee admin.txt; break;;
       [Nn]* ) exit;;
     esac
+    exit
   done
 fi
