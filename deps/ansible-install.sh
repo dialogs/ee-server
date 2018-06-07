f58eea9ccc79ab246784771655866a80a5588c95
Stanislav Kulbachnyy <sk@dlg.ru>
2018-06-07 12:51:43 +0300

    edits for Ubuntu-14 setup

--- deps/ansible-install.sh
+++ deps/ansible-install.sh
@@ -54,6 +54,9 @@ if [ ! $(which ansible-playbook) ]; then
 
     echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
 
+    # install certbot for letsencrypt
+    add-apt-repository -y ppa:certbot/certbot
+
     apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
     apt-get update
     apt-get install -y ansible
