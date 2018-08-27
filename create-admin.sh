49359806566ebea277c9fbf7301ad6da1ce8e11d
Recluse Freeman <me@recluse.ru>
2018-08-27 18:53:55 +0300

    Update create_admin.sh
    Remove unnecessary console flood

--- create-admin.sh
+++ create-admin.sh
@@ -9,9 +9,9 @@ fi
 id=`docker-compose exec postgresql su - postgres -c "echo \"select id,name from users where name='$admin_user'\" | psql -d dialog" | grep $admin_user | awk '{print $1}'` 2> /dev/null
 echo $id
 if [[ -z "$id" ]]; then
-  docker-compose exec dialog-server /opt/docker/bin/cli create-user -u $admin_user -n $admin_user
-  id=`docker-compose exec postgresql su - postgres -c "echo \"select id,name from users where name='$admin_user'\" | psql -d dialog" | grep $admin_user | awk '{print $1}'`
-  docker-compose exec dialog-server /opt/docker/bin/cli admin-grant -u $id | grep "Admin granted. Password: " | tee admin.txt
+  `docker-compose exec dialog-server /opt/docker/bin/cli create-user -u $admin_user -n $admin_user` 2> /dev/null
+  id=`docker-compose exec postgresql su - postgres -c "echo \"select id,name from users where name='$admin_user'\" | psql -d dialog" | grep $admin_user | awk '{print $1}'` 2> /dev/null
+  `docker-compose exec dialog-server /opt/docker/bin/cli admin-grant -u $id | grep "Admin granted. Password: " | tee admin.txt` 2> /dev/null
 else
   while true; do
     read -t 10 -p "User $1 was created. Do generate admin password? (y/n): " yn
@@ -22,3 +22,4 @@ else
     exit
   done
 fi
+exit 
